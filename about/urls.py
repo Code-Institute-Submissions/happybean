ccaa3e51d17306387e38e9e7ba4c7ac73cf5e352
Franciskadtt <franciskadtt@gmail.com>
2021-09-30 17:54:46 +0000

    Add contact us form and initial setup for email

--- about/urls.py
+++ about/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from .views import contactView, successView
+
 
 urlpatterns = [
-    path('', views.about, name='about'),
     path('our_story', views.our_story, name='our_story'),
     path('faq', views.faq, name='faq'),
-    path('contact_us', views.contact_us, name='contact_us'),
+    path('contact_us', contactView, name='contact_us'),
+    path('success/', successView, name='success'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+from django.contrib import messages
 
 
 def about(request):
@@ -20,8 +25,23 @@ def faq(request):
     return render(request, 'about/faq.html')
 
 
-def contact_us(request):
-    """
-    A view to return the contact_us page
-    """
-    return render(request, 'about/contact_us.html') 
+# Original Code with modifications from https://learndjango.com/tutorials/django-email-contact-form
+def contactView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "about/contact_us.html", {'form': form})
+
+
+def successView(request):
+    return render(request, 'about/contact_success.html')
