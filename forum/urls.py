f517b373fdcd30677b6f4cb1ddc4770cb090177b
Franciskadtt <franciskadtt@gmail.com>
2021-09-30 12:04:45 +0000

    Add the add thread template, view and url

--- forum/urls.py
+++ forum/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.forum, name='forum'),
+    path('add_thread', views.add_thread, name='add_thread'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def forum(request):
     """ A view to return the forum page """
     return render(request, 'forum/forum.html')
+
+
+def add_thread(request):
+    """ A view to return the add_thread page """
+    return render(request, 'forum/add_thread.html')
