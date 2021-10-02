8d5a02444a42238b9e086ad88044a44bb1b71416
Franciskadtt <franciskadtt@gmail.com>
2021-10-02 10:35:08 +0000

    Remove secret key

--- happybean/settings.py
+++ happybean/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=%!c#uyhf%pexh90ubd9xlt)mbt6*-)*6nwjkrn^q0e&-o8vdz'
+SECRET_KEY = os.environ.get('SECRET_KEY_MS4', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
