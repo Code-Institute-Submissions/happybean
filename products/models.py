8d139f9ca96e72ac34d1461f975f6b92bfd39561
Franciskadtt <franciskadtt@gmail.com>
2021-10-04 12:09:50 +0000

    Add Size classes to models in products app

--- products/models.py
+++ products/models.py
@@ -43,3 +43,29 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Size(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Size'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product_Size_Price(models.Model):
+    product = models.ForeignKey('Product', null=True, blank=True,
+                                on_delete=models.SET_NULL)
+    size = models.ForeignKey('Size', null=True, blank=True,
+                               on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
