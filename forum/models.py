872b4097f8f6bfd6e98ec1259c26e32b695b56c6
Franciskadtt <franciskadtt@gmail.com>
2021-10-12 18:05:14 +0000

    Add comments to models.py in forum app

--- forum/models.py
+++ forum/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+# ------------------------------------------- #
+#                THREAD MODEL                 #
+# ------------------------------------------- #
 class Thread(models.Model):
     """
     Model for threads in forum
@@ -13,16 +16,27 @@ class Thread(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_edited = models.DateTimeField(auto_now=True)
 
+    # Represents the class objects as a string
+    # To return a string of the thread title the creator of it
+    # https://www.python-course.eu/python3_magic_methods.php
     def __str__(self):
         return self.title + ' by ' + str(self.creator)
 
+    # To handle named arguments not yet defined from:
+    # https://docs.python.org/2/tutorial/controlflow.html#keyword-arguments
     def get_absolute_url(self):
         return reverse('thread', kwargs={'pk': self.pk})
 
 
+# ------------------------------------------- #
+#                COMMENT MODEL                #
+# ------------------------------------------- #
 class Comment(models.Model):
     """
-    Model for comments on a thread.
+    Model for comments related a thread. There field that links to
+    the Thread model by foreign key. It also links to the User
+    model with foreign key. There is also a field for the date
+    the comment was created and edited.
     """
     thread = models.ForeignKey(Thread, related_name="comments",
                                on_delete=models.CASCADE)
@@ -32,5 +46,5 @@ class Comment(models.Model):
     date_edited = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        # https://pyformat.info/#simple
-        return "On '%s' by %s" % (self.thread, self.creator)
+        # From: https://www.geeksforgeeks.org/python-string-format-method/
+        return 'On {} by {}'.format(self.thread, self.creator)
