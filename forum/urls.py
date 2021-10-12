f59b652bb8bf5be03d0fe1c8e1369c6c2311dc5e
Franciskadtt <franciskadtt@gmail.com>
2021-10-12 18:07:17 +0000

    Update urls.py in forum app for classed based views and add comments

--- forum/urls.py
+++ forum/urls.py
@@ -1,13 +1,29 @@
-from django.contrib import admin
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
 from . import views
+from .views import (
+    ThreadView,
+    AddThreadView,
+    EditThreadView,
+    DeleteThreadView,
+    AddCommentView,
+    EditCommentView,
+    DeleteCommentView,
+)
 
-
+# Class based views & urls https://bit.ly/3DyP7wf
 urlpatterns = [
-    path('', views.forum, name='forum'),
-    path('add_thread', views.add_thread, name='add_thread'),
-    path('thread', views.thread, name='thread'),
-    path('thread/add_comment/', views.add_comment, name='add_comment'),
+    path('', views.forum_view, name='forum'),
+    path('thread/<int:pk>', ThreadView.as_view(), name="thread"),
+    path('add_thread/', AddThreadView.as_view(), name="add_thread"),
+    path('thread/edit/<int:pk>', EditThreadView.as_view(), name="edit_thread"),
+    path('thread/delete/<int:pk>', DeleteThreadView.as_view(),
+         name="delete_thread"),
+    path('thread/<int:pk>/add_comment/', AddCommentView.as_view(),
+         name="add_comment"),
+    path('edit_comment/<int:pk>', EditCommentView.as_view(),
+         name="edit_comment"),
+    path('comment/delete/<int:pk>', DeleteCommentView.as_view(),
+         name="delete_comment")
 ]
