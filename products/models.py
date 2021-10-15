7bff066ccf6d23db704a645a6c70c7a7e01074ba
Franciskadtt <franciskadtt@gmail.com>
2021-10-15 11:44:45 +0000

    Update product models to remove size

--- products/models.py
+++ products/models.py
@@ -58,50 +58,10 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     intensity = models.CharField(max_length=20, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-# ------------------------------------------- #
-#               SIZE MODEL                    #
-# ------------------------------------------- #
-class Size(models.Model):
-    """
-    Model for the size names of products.
-    """
-
-    class Meta:
-        verbose_name_plural = 'Size'
-
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
-
-
-# ------------------------------------------- #
-#           PRODUCT, SIZE & PRICE MODEL       #
-# ------------------------------------------- #
-class ProductSizePrice(models.Model):
-    """
-    Model for the linking products to specific pricing related to
-    their sizes.
-    """
-    product = models.ForeignKey('Product', null=True, blank=True,
-                                on_delete=models.SET_NULL)
-    size = models.ForeignKey('Size', null=True, blank=True,
-                               on_delete=models.SET_NULL)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
