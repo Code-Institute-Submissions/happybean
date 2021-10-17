761586d81e3374c6fede3605826fe386624c30cc
Franciskadtt <franciskadtt@gmail.com>
2021-10-17 15:28:05 +0000

    Add login requirement for coffee corner app

--- coffee_corner/views.py
+++ coffee_corner/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def coffee_corner(request):
     """ A view to return the coffee_corner page """
+    if not request.user.is_superuser:
+        messages.error(request, 'You need to have the correct '
+                       'permissions to manage product details')
+        return redirect(reverse('login'))
+
     return render(request, 'coffee_corner/coffee_corner.html')
