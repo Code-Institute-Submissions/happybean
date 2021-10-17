3dfda6dfb13c56b06834dd17b121a388d3edb6df
Franciskadtt <franciskadtt@gmail.com>
2021-10-17 17:07:16 +0000

    Resolve pylint errors and warnings

--- about/urls.py
+++ about/urls.py
@@ -1,13 +1,15 @@
-from django.contrib import admin
+"""
+Urls for About Pages including Our Story, FAQ & Contact Form
+"""
+
 from django.urls import path
 from . import views
-
-from .views import contactView, successView
+from .views import contact_view, success_view
 
 
 urlpatterns = [
     path('our_story', views.our_story, name='our_story'),
     path('faq', views.faq, name='faq'),
-    path('contact_us', contactView, name='contact_us'),
-    path('success/', successView, name='success'),
+    path('contact_us', contact_view, name='contact_us'),
+    path('success/', success_view, name='success'),
 ]
