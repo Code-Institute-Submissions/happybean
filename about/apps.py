3f146fd4e6cf676aec40562781f49cdd60be695f
Franciskadtt <franciskadtt@gmail.com>
2021-10-19 16:28:28 +0000

    Resolve pylint issuse in about app

--- about/apps.py
+++ about/apps.py
@@ -1,6 +1,12 @@
+"""
+Configure About application
+"""
 from django.apps import AppConfig
 
 
 class AboutConfig(AppConfig):
+    """
+    About App Configuration
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'about'
