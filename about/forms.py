a79461065036b5985080c3ff0a13255641681d2a
Franciskadtt <franciskadtt@gmail.com>
2021-10-19 14:30:43 +0000

    Resolve problems in about app python files

--- about/forms.py
+++ about/forms.py
@@ -1,6 +1,13 @@
+"""
+Contact Form in About App
+"""
 from django import forms
 
+
 class ContactForm(forms.Form):
+    """
+    Contact Form fields for Contact Form in About App
+    """
     from_email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
