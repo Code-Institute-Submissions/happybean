43daff6af4fd60f6b6a0dacffba8cca882fa9173
Franciskadtt <franciskadtt@gmail.com>
2021-10-19 16:28:16 +0000

    Resolve pylint issuse in cart app

--- cart/apps.py
+++ cart/apps.py
@@ -1,6 +1,12 @@
+"""
+Configure Cart application
+"""
 from django.apps import AppConfig
 
 
 class CartConfig(AppConfig):
+    """
+    Cart App Configuration
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cart'
