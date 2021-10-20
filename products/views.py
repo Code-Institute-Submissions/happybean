bb53ad1713a938090166199127209299043dc683
Franciskadtt <franciskadtt@gmail.com>
2021-10-20 13:59:19 +0000

    Resolve pylint issues

--- products/views.py
+++ products/views.py
@@ -12,9 +12,6 @@ from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
-    DetailView,
-    CreateView,
-    UpdateView,
     DeleteView,
 )
 
