80f60fbd0223453094992b35b0b9597151a32696
Franciskadtt <franciskadtt@gmail.com>
2021-10-20 14:53:51 +0000

    Update deleting products url

--- products/urls.py
+++ products/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-    path('products/delete/<int:pk>', DeleteProductView.as_view(),
+    path('delete/<int:pk>', DeleteProductView.as_view(),
          name="delete_product"),
 ]
